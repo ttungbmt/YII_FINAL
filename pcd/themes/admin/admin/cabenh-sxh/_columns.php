6d6a055ced62f4469d86ec167f409cdf1c8511f5
Truong Thanh Tung <ttungbmt@gmail.com>
2020-07-02 11:31:08 +0700

    Update _columns.php

--- pcd/themes/admin/admin/cabenh-sxh/_columns.php
+++ pcd/themes/admin/admin/cabenh-sxh/_columns.php
@@ -58,7 +58,7 @@ return [
         'updateOptions' => ['title' => lang('Update'), 'data-toggle' => 'tooltip'],
         'visibleButtons' => [
             'delete' => function ($model) {
-                return role('admin|quan') || userInfo()->maphuong === '76526953';
+                return role('admin|quan');
             },
             'view' => false,
             'download' => function ($model) {
